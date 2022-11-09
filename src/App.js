61b4f03d1850248cac9dc3055eba37079da60c43
Cicero Feijo <cicero.feijo@gmail.com>
2022-11-09 15:19:24 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <h1>Create React Template - Updated from CLI</h1>
+    <h1>Create React Template - Updated from Vdercel CLI</h1>
   )
 }
 
